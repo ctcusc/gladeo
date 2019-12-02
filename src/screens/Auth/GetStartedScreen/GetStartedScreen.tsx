dc330dc4ae6c238825e8944a0bb259c06b16ad52
Xiaoyang Qiao <xiaoyanq@usc.edu>
2019-12-02 00:39:00 -0800

    create GetStartedScreen

---
+++ src/screens/Auth/GetStartedScreen/GetStartedScreen.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import {
+  Text,
+  View,
+  Alert,
+  TouchableOpacity,
+  Image
+} from 'react-native'
+import styles from '../../Auth/GetStartedScreen/styles';
+import BlackHeading from '../../../shared_components/BlackHeading/BlackHeading';
+import GreyTextInput from '../../../shared_components/GreyTextInput/GreyTextInput';
+import PinkButton from '../../../shared_components/PinkButton/PinkButton';
+
+export default function GetStartedScreen() {
+    const [title, setTitle] = useState('')
+    const [code, setCode] = useState('')
+
+    function changeTitle(title: string){
+        setTitle(title)
+    }
+
+    function changeCode(code: string){
+        setCode(code)
+    }
+
+  return (
+    <View style={styles.container}>
+        <View style={styles.main}>
+            <BlackHeading title="Let's get Started!" />
+            <Text style={styles.title}></Text>
+            <GreyTextInput changeTextContent={changeTitle} placeholder="Current Title" inputType='text'/>
+            <GreyTextInput changeTextContent={changeCode} placeholder="Company Code" inputType='text'/>
+            <PinkButton title="START CREATING" onPress={() =>  Alert.alert('pressed')} />
+        </View>
+
+        <View style={styles.footer}>
+            <View style={styles.resendButtonLine}> 
+                <Text style={styles.normalText}>Already have an account?</Text>
+                <TouchableOpacity onPress={() => Alert.alert('pressed')}>
+                    <Text style={styles.pinkTextButton}> Sign In</Text>
+                </TouchableOpacity>
+            </View>
+            <Image style={styles.image} resizeMode='contain' source={require('../../../../assets/images/gladeo_logo.png')} />
+        </View>
+    </View>
+  )
+}
---
+++ src/screens/Auth/GetStartedScreen/styles.tsx
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native'
+import { normalize } from 'path'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+
+    main: {
+        flex: 3,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        color: '#000',
+    },
+
+    title: {
+        marginBottom: '16%',
+    },
+
+    normalText: {
+        fontStyle: 'normal',
+        fontFamily: 'roboto-regular',
+        fontSize: 16,
+        lineHeight: 19,
+        color: '#000000',
+    },
+
+    pinkTextButton: {
+        fontStyle: 'normal',
+        fontSize: 16,
+        lineHeight: 19,
+        fontFamily: 'roboto-regular',
+        color: '#E5186E',
+    },
+
+    resendButtonLine: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '2.3%'
+    },
+
+    image: {
+        height: 95,
+    },
+
+    footer: {
+        flex: 1
+    }
+})
+
+export default styles
