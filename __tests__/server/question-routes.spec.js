4ee3b1fa9e95c0128271195aab4db40fe79af565
vikram-kher <vikramkher@live.com>
2019-12-02 18:08:46 -0800

    VK - test creation

--- __tests__/server/question-routes.spec.js
+++ __tests__/server/question-routes.spec.js
@@ -2,9 +2,17 @@ const supertest = require('supertest')
 const app = require('../../server/server')
 const request = supertest(app)
 
-describe('[unit of work]', () => {
-  it('should [expected behaviour] when [scenario/context]', async done => {
+describe('GET /api/questions', () => {
+  it('should print questions in json format when request is successful', async done => {
     const res = await request.get('/api/questions')
     done()
   });
 });
+
+describe('GET invalid url', () => {
+  it('should return 404 error when invalid url is used', async done => {
+    const res = await request.get('/api/invalidurl')
+    done()
+  });
+});
+
