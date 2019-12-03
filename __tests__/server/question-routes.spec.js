b85bbcaef88802418fc14edb2c380aa128277881
Chuanxiu Xiong <chuanxiuxiong@outlook.com>
2019-12-03 11:11:51 -0800

    add expect

--- __tests__/server/question-routes.spec.js
+++ __tests__/server/question-routes.spec.js
@@ -1,18 +1,21 @@
-const supertest = require('supertest')
-const app = require('../../server/server')
-const request = supertest(app)
+const supertest = require('supertest');
+const app = require('../../server/server');
+const request = supertest(app);
 
 describe('GET /api/questions', () => {
-  it('should print questions in json format when request is successful', async done => {
-    const res = await request.get('/api/questions')
-    done()
+  it('should print questions in json format when request is successful', async () => {
+    const res = await request.get('/api/questions');
+    // The current bruteforce way of checking it is to see if there are more than 8 questions returned.
+    // TODO: should change the following after we implement the create question function.
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBeGreaterThan(8);
   });
 });
 
 describe('GET invalid url', () => {
-  it('should return 404 error when invalid url is used', async done => {
-    const res = await request.get('/api/invalidurl')
-    done()
+  it('should return 404 error when invalid url is used', async () => {
+    const res = await request.get('/api/invalidurl');
+    expect(res.status).toBe(404);
   });
 });
 
