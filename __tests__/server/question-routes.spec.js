cfe0c17f28ea1c6af9bfe90b15a8b439c1e0577f
vikram-kher <vikramkher@live.com>
2019-12-04 13:21:26 -0800

    Added additional tests per PR Review

--- __tests__/server/question-routes.spec.js
+++ __tests__/server/question-routes.spec.js
@@ -2,20 +2,37 @@ const supertest = require('supertest');
 const app = require('../../server/server');
 const request = supertest(app);
 
-describe('GET /api/questions', () => {
+describe('Checks to see if questions route returns the list of questions', () => {
   it('should print questions in json format when request is successful', async () => {
     const res = await request.get('/api/questions');
-    // The current bruteforce way of checking it is to see if there are more than 8 questions returned.
-    // TODO: should change the following after we implement the create question function.
     expect(res.status).toBe(200);
     expect(res.body.length).toBeGreaterThan(8);
   });
 });
 
-describe('GET invalid url', () => {
+describe('Checks to see if an invalid route is handled', () => {
   it('should return 404 error when invalid url is used', async () => {
     const res = await request.get('/api/invalidurl');
     expect(res.status).toBe(404);
   });
 });
 
+describe('Checks if first question returned matches the desired string', () => {
+  it('should print questions in json format when request is successful', async () => {
+    const res = await request.get('/api/questions');
+    expect(res.status).toBe(200);
+    expect(res.body[0].text).toMatch('Who/what influenced or inspired you to do what you do?')
+  });
+});
+
+describe('Checks to see if id/text members exist for each question', () => {
+  it('should print questions in json format when request is successful', async () => {
+    const res = await request.get('/api/questions');
+    expect(res.status).toBe(200);
+    for (var i = 0; i < res.body.length; i++) {
+      var keys = Object.keys(res.body[i]);
+      expect(keys[0]).toMatch('id');
+      expect(keys[1]).toMatch('text');
+    }
+  });
+});
