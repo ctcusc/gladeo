bd9df5b0c465715dba8ee655e666d7682c84a10a
Xiaoyang Qiao <xiaoyanq@usc.edu>
2019-12-15 10:48:35 -0800

    add sub-stepScreens

--- src/screens/Home/OnboardingScreen/StepScreens.tsx
+++ src/screens/Home/OnboardingScreen/StepScreens.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
   NavigationScreenProp, 
   NavigationState
@@ -14,20 +14,50 @@ import styles from './stepStyles'
 /* AKA: Q&A screen */
 export default function OnboardingScreen(props: Props) {
   const {navigate} = props.navigation
+  const [step, setStep] = useState(2)
+  const [text, setText] = useState('Select questions to answer')
+  const [header, setHeader] = useState('Step 1')
+  const [textStyle, setTextStyle] = useState(styles.stepOne)
+
   return (
     <View style={styles.container}>
-        <View style={styles.main}>
-            <Text style={styles.step}>Step 1</Text>
-            <Text style={styles.text}>Select questions to answer</Text>
-            <Image style={styles.image} resizeMode='contain' source={require('../../../../assets/images/rectangle.png')} />
-            <Image style={styles.image} resizeMode='contain' source={require('../../../../assets/images/rectangle.png')} />
-            <Image style={styles.image} resizeMode='contain' source={require('../../../../assets/images/rectangle.png')} />
-        </View>
-        <View style={styles.footer}>
-            <TouchableOpacity onPress={() => navigate('Home')} style={styles.button}>
-                <Text style={styles.buttonText}>Next</Text>
-            </TouchableOpacity>
-        </View>
+      <View style={styles.main}>
+        <Text style={styles.header}>{header}</Text>
+        <Text style={textStyle}>{text}</Text>
+        <Image style={styles.image} resizeMode='contain' source={require('../../../../assets/images/rectangle.png')} />
+        <Image style={styles.image} resizeMode='contain' source={require('../../../../assets/images/rectangle.png')} />
+        <Image style={styles.image} resizeMode='contain' source={require('../../../../assets/images/rectangle.png')} />
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity 
+          onPress={() => {
+            setStep(step + 1)
+            if(step == 2) {
+              setTextStyle(styles.stepTwo)
+              setText('Record your responses as video snippets')
+              setHeader('Step 2')
+            }
+            if(step == 3) {
+              setTextStyle(styles.stepThree)
+              setText('Choose snippets to include in your video')
+              setHeader('Step 3')
+            }
+            if(step == 4) {
+              setTextStyle(styles.stepFour)
+              setText('Create + publish your video to Gladeo\'s Youtube')
+              setHeader('Step 4')
+            }
+            if(step == 5){
+              navigate('Home')
+            }
+          } 
+          }
+          style={styles.button}>
+          <Text style={styles.buttonText}>
+              Next
+          </Text>
+        </TouchableOpacity>
+      </View>
       
     </View>
   )
--- src/screens/Home/OnboardingScreen/stepStyles.tsx
+++ src/screens/Home/OnboardingScreen/stepStyles.tsx
@@ -1,65 +1,105 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#D94077',
-      },
+  container: {
+    flex: 1,
+    backgroundColor: '#D94077',
+  },
     
-    main: {
-        flex: 2,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  main: {
+    flex: 2,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-    footer: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  footer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-    step: {
-        color: '#DEA3BB',
-        fontFamily: 'montserrat-regular', 
-        fontWeight: 'bold',
-        fontSize: 30,
-        marginBottom: '10%',
+  header: {
+    color: '#DEA3BB',
+    fontFamily: 'montserrat-regular', 
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginBottom: '10%',
+  },
+
+  stepOne: {
+    fontFamily: 'montserrat-regular', 
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 30,
+    lineHeight: 37,
+    textAlign: 'center',
+    color: '#F5F5F5',
+    marginBottom: '20%',
+    height: 74,
+    width: 256,
+  }, 
+
+  stepTwo: {
+    fontFamily: 'montserrat-regular', 
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 30,
+    lineHeight: 37,
+    textAlign: 'center',
+    color: '#F5F5F5',
+    marginBottom: '11%',
+    height: 111,
+    width: 256,
+  }, 
+
+  stepThree: {
+    fontFamily: 'montserrat-regular', 
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 30,
+    lineHeight: 37,
+    textAlign: 'center',
+    color: '#F5F5F5',
+    marginBottom: '11%',
+    height: 111,
+    width: 270,
+  }, 
+
+  stepFour: {
+    fontFamily: 'montserrat-regular', 
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 30,
+    lineHeight: 37,
+    textAlign: 'center',
+    color: '#F5F5F5',
+    marginBottom: '11%',
+    height: 111,
+    width: 307,
+  }, 
+
+  image: {
+    marginBottom: '3%',
+  },
+
+  button: {
+    width: 179,
+    height: 50,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 8,
+    justifyContent: 'center',
+  },
     
-    },
-
-    text: {
-        fontFamily: 'montserrat-regular', 
-        fontSize: 30,
-        lineHeight: 37,
-        fontWeight: 'bold',
-        color: '#F5F5F5',
-        marginBottom: '20%',
-        textAlign: 'center',
-        width: 256,
-    }, 
-
-    image: {
-        marginBottom: '3%',
-    },
-
-    button: {
-        width: 179,
-        height: 50,
-        backgroundColor: '#FFFFFF',
-        borderRadius: 8,
-        justifyContent: 'center',
-    },
-    
-    buttonText: {
-        fontStyle: 'normal',
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: '#E5186E',
-        textAlign: 'center',
-    }
+  buttonText: {
+    fontStyle: 'normal',
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#E5186E',
+    textAlign: 'center',
+  }
 })
 
 export default styles
