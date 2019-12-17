eabaac28daffcab3d9dec9984187d9d6aaf97307
brennachenbrennachen <brenna.chen@gmail.com>
2019-12-16 17:39:20 -0800

    Added video loading screen

---
+++ src/screens/Home/CreateScreen/VideoLoadingScreen.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import AnimatedEllipsis from 'react-native-animated-ellipsis';
+import {
+  Text,
+  View,
+} from 'react-native'
+import styles from './styles'
+
+export default function CreateScreen() {
+  return (
+    <View style={styles.container}>
+        <View style={styles.main}>
+            <AnimatedEllipsis numberOfDots={4}
+                  minOpacity={0.5}
+                  animationDelay={200}
+                  style={styles.ellipses}
+            />
+            <Text style={styles.text}>CREATING YOUR VIDEO</Text>
+        </View>
+    </View>
+  )
+}
+
+
--- src/screens/Home/CreateScreen/styles.tsx
+++ src/screens/Home/CreateScreen/styles.tsx
@@ -13,6 +13,29 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 40,
   },
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ellipses: {
+      color: '#E5186E',
+      fontSize: 200,
+      letterSpacing: -50,
+  },
+  text: {
+    fontFamily: 'montserrat-regular', 
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 30,
+    lineHeight: 37,
+    textAlign: 'center',
+    color: '#E5186E',
+    height: 114,
+    width: 307,
+    marginBottom: '45%',
+  },
 })
 
 export default styles
