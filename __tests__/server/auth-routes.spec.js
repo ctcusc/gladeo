ba1a69dde62abf12914557cb734ca74c8ce8b5fa
Xiaoyang Qiao <xiaoyanq@usc.edu>
2020-01-14 16:18:13 -0800

    modify the test cases

--- __tests__/server/auth-routes.spec.js
+++ __tests__/server/auth-routes.spec.js
@@ -1,34 +1,53 @@
 const supertest = require('supertest')
 const app = require('../../server/api/app')
 const request = supertest(app)
-const { getUser } = require('../../data_access_layer/user')
+const { base } = require('./index')
+
 
 // check invalid URL
 describe('Checks to see if an invalid route is handled', () => {
-  it('should return 404 error when invalid url is used', async() => {
-    const res = await request.get('/api/auth/invalidURL')
+  it('should return 404 error when invalid url is used', async done => {
+    const res = await request.post('/api/auth/invalidURL').send({
+      'Full Name': 'Aliya Petranik',
+      'Email': 'petranik@usc.edu',
+      'Current Title': 'Tech Lead',
+      'Company': '1', 
+    })
     expect(res.status).toBe(404)
+    done()
   })
 })
 
 // check adding a user that already exists
 describe('Checks to see if adding a user that already exists is handled', () => {
-  it('should return 409 error', async() => {
-    const res = await request.get('/api/auth/register')
-    if(res.status === 409) {
-      const user = await getUser(res.body['Email'])
-      expect(user).toBeDefined
-    }
+  it('should return 409 error', async done => {
+    const res = await request.post('/api/auth/register').send({
+      'Full Name': 'Aliya Petranik',
+      'Email': 'petranik@usc.edu',
+      'Current Title': 'Tech Lead',
+      'Company': '1', 
+    })
+    expect(res.status).toBe(409)
+    done()
   })
 }) 
 
 // check successfully adding a user
-describe('Checks to see if a new user is created successfully', () => [
-  it('should return 200 and successfully create a user', async() => {
-    if(res.status === 200) {
-      const user = await getUser(res.body['Email'])
-      expect(user).toBeDefined
-    }
+describe('Checks to see if a new user is created successfully', () => {
+  it('should return 200 and successfully create a user', async done => {
+    const res = await request.post('api/auth/register').send({
+      'Full Name': 'Yang Qiao',
+      'Email': 'xiaoyanq@usc.edu',
+      'Current Title': 'Developer',
+      'Company': '1', 
+    })
+    expect(res.status).toBe(202)
+    expect(res.body['Full Name']).toBeTruthy()
+    expect(res.body['Email']).toBeTruthy()
+    done()
   })
-])
-  
+
+  afterAll(async() => {
+    await base('User').drop()
+  })
+})  
