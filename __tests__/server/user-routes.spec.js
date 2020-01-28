1c0355a4c438ba908991514b0d83b96fcedb9f4b
Lynne <lynnebai@usc.edu>
2020-01-28 00:15:47 -0800

    use session

--- __tests__/server/user-routes.spec.js
+++ __tests__/server/user-routes.spec.js
@@ -4,40 +4,16 @@ const request = supertest(app)
 const { clearFieldsInSingleRecord } = require('../../server/data_access_layer/helpers')
 const { getUserByEmail } = require('../../server/data_access_layer/user')
 const session = require('express-session')
+const supertestsession = require('supertest-session')
 
-const user1ID = 1 // `ID` field in `user` table
-const user2Id = 'recaHw82NAck0EquE'
-const user2ID = 2
+const user2Id = 'rec2r04V2X5oxB9dd'
 const userBaseName = 'Users'
 
-// keep here or helpers.js ??
-async function checkAuth(req, res) {
-  if (!req.session.data) {
-    return res.status(401).json()
-  }
-  const { email, password } = req.session.data
-  const user = await getUserByEmail(email)
-  //return res.status(200).json({ email, password });
-  return res.status(200).json(user)
-}
-async function logout(req, res) {
-  req.session.authenticated = null
-  return res.status(200).json()
-}
-const mockRequest = (sessionData) => {
-  return {
-    session: { data: sessionData },
-  }
-}
-const mockResponse = () => {
-  const res = {}
-  res.status = jest.fn().mockReturnValue(res)
-  res.json = jest.fn().mockReturnValue(res)
-  return res
-}
-// **************
+let testSession = null
+beforeEach(function () {
+  testSession = supertestsession(app)
+})
 
-/*
 describe('Checks user answered routes', () => {
   const answeredFieldName = 'Answered'
   let questions
@@ -48,42 +24,41 @@ describe('Checks user answered routes', () => {
     expect(questionsRes.status).toBe(200)
     questions = questionsRes.body
     questionIDs = questions.map(question => question.ID)
-
+    
   })
-  // the following test might modify `answered` field of user2
+  // the following test might modify `answered` field of user test2
   beforeEach(async () => {
     clearFieldsInSingleRecord(userBaseName, user2Id, answeredFieldName)
   })
-  it('should return status 404 when nonexistent userId is given', async () => {
-    const res = await request.get(`/api/user/${-1}/answered`)
-    expect(res.status).toBe(404)
-  })
-  // assume user1 always have at least one answered questions
-  it('should return the nonempty array of the answered questions of user1', async () => {
-    const res = await request.get(`/api/user/${user1ID}/answered`)
+  // assume user always has at least one answered questions
+  it('should return the nonempty array of the answered questions of user test', async () => {
+    await testSession.post('/api/auth/login').send({ Email: 'a@b.com', Password: 'password'})
+    const res = await testSession.get('/api/user/answered')
     expect(res.status).toBe(200)
     const answeredQuestions = res.body
     expect(res.body.length).toBeGreaterThan(0)
     // check if each of the recordId is one existing questionId
     answeredQuestions.forEach(question => expect(questionIDs.includes(question.ID)).toBe(true))
   })
-  it('should return the empty array of the answered questions of user2', async () => {
-    const res = await request.get(`/api/user/${user2ID}/answered`)
+  it('should return the empty array of the answered questions of user test2', async () => {
+    await testSession.post('/api/auth/login').send({ Email: 'a2@b.com', Password: 'password'})
+    const res = await testSession.get('/api/user/answered')
     expect(res.status).toBe(200)
     expect(res.body.length).toBe(0)
   })
   it('should return updated ids of the answered questions when multiple questions are added sequentially', async () => {
+    await testSession.post('/api/auth/login').send({ Email: 'a2@b.com', Password: 'password'})
     let res
-    res = await request.get(`/api/user/${user2ID}/answered`)
+    res = await testSession.get('/api/user/answered')
     expect(res.status).toBe(200)
     expect(res.body.length).toBe(0)
     const numOfQuestionsAdded = Math.min(4, questions.length)
     for (let i = 0; i < numOfQuestionsAdded; ++i) {
-      res = await request.post(`/api/user/${user2ID}/answer`).send({ questionId: questionIDs[i]})
+      res = await testSession.post('/api/user/answer').send({ questionId: questionIDs[i]})
       expect(res.status).toBe(200)
     }
     setTimeout(async () => {
-      res = await request.get(`/api/user/${user2ID}/answered`)
+      res = await testSession.get('/api/user/answered')
       expect(res.status).toBe(200)
       expect(res.body.length).toBe(numOfQuestionsAdded)
       const answered = res.body.Answered
@@ -93,13 +68,14 @@ describe('Checks user answered routes', () => {
     }, (3)) // needs a little time for the change go through the DB
   })
   it('should return updated user w/ new question in answered field', async () => {
+    await testSession.post('/api/auth/login').send({ Email: 'a2@b.com', Password: 'password'})
     let res
-    res = await request.get(`/api/user/${user2ID}/answered`)
+    res = await testSession.get('/api/user/answered')
     expect(res.status).toBe(200)
-    res = await request.post(`/api/user/${user2ID}/answer`).send({ questionId: questionIDs[7]})
+    res = await testSession.post('/api/user/answer').send({ questionId: questionIDs[7]})
     expect(res.status).toBe(200)
     setTimeout(async () => {
-      res = await request.get(`/api/user/${user2ID}/answered`)
+      res = await testSession.get('/api/user/answered')
       expect(res.status).toBe(200)
       expect(res.body.length).toBe(1)
       const answered = res.body.Answered
@@ -109,37 +85,25 @@ describe('Checks user answered routes', () => {
   afterAll(async () => {
     clearFieldsInSingleRecord(userBaseName, user2Id, answeredFieldName)
   })
-}) */
+}) 
 
 describe('Checks to see if user data is correct', () => {
-  it('should 401 if session data is not set', async () => {
-    const req = mockRequest()
-    const res = mockResponse()
-    await checkAuth(req, res)
-    expect(res.status).toHaveBeenCalledWith(401)
+  it('should 404 if no user is logged in', async () => {
+    const res = await testSession.get('/api/user')
+    expect(res.status).toBe(404)
   })
-  it('should 200 with username from session if session data is set', async () => {
-    const req = mockRequest({ email: 'a@b.com', password: 'password'})
-    const res = mockResponse()
-    await checkAuth(req, res)
-    expect(res.status).toHaveBeenCalledWith(200)
-    expect(res.json).toHaveBeenCalledWith({ email: 'a@b.com', password: 'password' })
-    // expect(res.json['Full Name']).toMatch('test')
-    // expect(res.json['Email']).toMatch('a@b.com')
-    // expect(res.json['Current Title']).toMatch('Dev')
-    // expect(res.status).toBe(200)
-  })
-  it('should print the first user data if successful', async () => {
-    //req.session.authenticated = user
-
-    const res = await request.get('/api/user')
+  it('should print the data of current user if logged in', async () => {
+    await testSession.post('/api/auth/login').send({ Email: 'a@b.com', Password: 'password'})
+    const res = await testSession.get('/api/user')
+    
     expect(res.body['Full Name']).toMatch('test')
     expect(res.body['Email']).toMatch('a@b.com')
     expect(res.body['Current Title']).toMatch('Dev')
     expect(res.status).toBe(200)
   })
   it('should return Full Name/Email/Current Title/Answered members if successful', async () => {
-    const res = await request.get('/api/user')
+    await testSession.post('/api/auth/login').send({ Email: 'a@b.com', Password: 'password'})
+    const res = await testSession.get('/api/user')
     const keys = Object.keys(res.body)
     expect(keys[0]).toMatch('_record')
     expect(keys[1]).toMatch('Current Title')
@@ -148,7 +112,8 @@ describe('Checks to see if user data is correct', () => {
     expect(keys[4]).toMatch('Company')
     expect(keys[5]).toMatch('Email')
     expect(keys[6]).toMatch('ID')
-    expect(keys[7]).toMatch('Answered')
+    if(keys[7])
+      expect(keys[7]).toMatch('Answered')
     expect(res.status).toBe(200)
   })
 })
--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { getUser, updateAnsweredQuestions } = require('../../data_access_layer/user')
+const { updateAnsweredQuestions } = require('../../data_access_layer/user')
 const { getQuestion } = require('../../data_access_layer/question')
 
 router.get('/', async (req, res) => {
@@ -15,32 +15,35 @@ router.get('/', async (req, res) => {
 
 // Returns array of all questions user has answered
 router.get('/answered', async (req, res) => {
-  if(req.session && req.session.authenticated) { // user logged in
-    const user = req.session.authenticated
-    /* 
-      User table has foreign key in 'Answered' column that holds reference ID to question table, not primary key
-      Use getQuestion() to extract ID and text from Question table given reference ID
-    */
-    const answeredQuestions = user.Answered
-    if (answeredQuestions === undefined) {
-      return res.status(200).send([])
+  try {
+    if(req.session && req.session.authenticated) { // user logged in
+      const user = req.session.authenticated
+      /* 
+        User table has foreign key in 'Answered' column that holds reference ID to question table, not primary key
+        Use getQuestion() to extract ID and text from Question table given reference ID
+      */
+      const answeredQuestions = user.Answered
+      if (answeredQuestions === undefined) {
+        return res.status(200).send([])
+      }
+      const extractQuestions = () => {
+        return Promise.all(answeredQuestions.map(question => getQuestion(question)))
+      }
+      extractQuestions().then(data => {
+        return res.status(200).send(data)
+      })
     }
-    const extractQuestions = () => {
-      return Promise.all(answeredQuestions.map(question => getQuestion(question)))
+  } catch (err) {
+    // when `statusCode` is not included, it is a server error 500
+    if (err.statusCode === undefined) {
+      return res.status(500).send({
+        statusCode: 500,
+        message: err.message,
+        stack: err.stack
+      })
     }
-    extractQuestions().then(data => {
-      return res.status(200).send(data)
-    })
+    return res.status(err.statusCode).send(err)
   }
-  // when `statusCode` is not included, it is a server error 500
-  if (err.statusCode === undefined) {
-    return res.status(500).send({
-      statusCode: 500,
-      message: err.message,
-      stack: err.stack
-    })
-  }
-  return res.status(err.statusCode).send(err)
 })
 
 /* 
@@ -49,37 +52,40 @@ router.get('/answered', async (req, res) => {
   Returns the updated User
 */
 router.post('/answer', async (req, res) => {
-  if(req.session && req.session.authenticated) { // user logged in
-    const user = req.session.authenticated
-    const { questionId } = req.body
-    const answeredQuestions = [] // ensures array is at least defined if it is empty
-    if (user.Answered !== undefined) {
-      answeredQuestions.concat(user.Answered) // already answered by user
+  try {
+    if(req.session && req.session.authenticated) { // user logged in
+      const user = req.session.authenticated
+      const { questionId } = req.body
+      const answeredQuestions = [] // ensures array is at least defined if it is empty
+      if (user.Answered !== undefined) {
+        answeredQuestions.concat(user.Answered) // already answered by user
+      }
+
+      // Use question's ID to grab the whole object from questions table
+      const newQuestion = await getQuestion(questionId)
+
+      // question already answered - don't let dups through
+      if (answeredQuestions.includes(newQuestion._record)) {
+        return res.status(200).send(user)
+      }
+      answeredQuestions.push(newQuestion._record)
+
+      // add new question to user's answered list
+      const updatedUser = await updateAnsweredQuestions(user, answeredQuestions)
+
+      return res.status(200).send(updatedUser)
+    } 
+  } catch (err) {
+    // when `statusCode` is not included, it is a server error 500
+    if (err.statusCode === undefined) {
+      return res.status(500).send({
+        status: 500,
+        message: err.message,
+        stack: err.stack
+      })
     }
-
-    // Use question's ID to grab the whole object from questions table
-    const newQuestion = await getQuestion(questionId)
-
-    // question already answered - don't let dups through
-    if (answeredQuestions.includes(newQuestion._record)) {
-      return res.status(200).send(user)
-    }
-    answeredQuestions.push(newQuestion._record)
-
-    // add new question to user's answered list
-    const updatedUser = await updateAnsweredQuestions(user, answeredQuestions)
-
-    return res.status(200).send(updatedUser)
+    return res.status(err.statusCode).send(err)
   }
-  // when `statusCode` is not included, it is a server error 500
-  if (err.statusCode === undefined) {
-    return res.status(500).send({
-      status: 500,
-      message: err.message,
-      stack: err.stack
-    })
-  }
-  return res.status(err.statusCode).send(err)
 })
 
 module.exports = router
