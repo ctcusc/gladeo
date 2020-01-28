183822aab4d598db35114f07d61059c31229189d
Jamie Flores <jamieflores@usc-securewireless-student-new46.usc.edu>
2020-01-28 11:55:47 -0800

    Fixed some linter errors

--- src/screens/Home/RecordScreen/RecordScreen.tsx
+++ src/screens/Home/RecordScreen/RecordScreen.tsx
@@ -2,16 +2,34 @@ import React, { useState, useEffect } from 'react'
 import { Text, View, TouchableOpacity } from 'react-native'
 import { Camera } from 'expo-camera'
 import styles from './styles'
-import { NavigationScreenProp, NavigationState } from 'react-navigation'
+import { FontAwesome } from '@expo/vector-icons'
+import { StopWatch } from 'react-native-stopwatch-timer'
+
+// Note: This page is meant to be displayed in landscape, so components are ordered left to right
+// instead of top to bottom. 
 
 interface Props {
   question: string,
+}
+
+const options = {
+  container: {
+    backgroundColor: 'rgba(51, 51, 51, 0.4)',
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'roboto-regular',
+    fontSize: 16,
+  }
 }
 
 export default function RecordScreen(props: Props) {
   const [hasPermission, setHasPermission] = useState(false)
   const [camera, setCamera] = useState()
   const [isRecording, setIsRecording] = useState(false)
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.front)
 
   useEffect(() => {
     (async () => {
@@ -32,33 +50,80 @@ export default function RecordScreen(props: Props) {
         ref={(ref: Camera) => {
           setCamera(ref)
         }}
-        type={Camera.Constants.Type.front}
+        type={cameraType}
       >
         <View>
-          <View style={{height: 50}}/>
-          <TouchableOpacity
-            onPress={ () => {
-              if(camera) {
-                if (isRecording) {
-                  setIsRecording(false)
-                  camera.stopRecording()
-                } else {
-                  const video = camera.recordAsync()
-                  setIsRecording(true)
-                }
-              }
-            }}
-            style={styles.recordButton}
-          >
-          </TouchableOpacity>
-          {/* Temporary recording indicator */}
           <View>
-            {
-              isRecording && <Text>RECORDING IN PROGRESS...</Text>
-            }
+            <View style={styles.photoButtonCircle}>
+              <TouchableOpacity
+                onPress={ async () => {
+                  const photo = await camera.takePictureAsync()
+                }}
+                style={styles.photoButton}  
+              />
+            </View>
+            <View style={styles.recordButtonCircle}/>
+            <TouchableOpacity
+              onPress={ () => {
+                if(camera) {
+                  if (isRecording) {
+                    setIsRecording(false)
+                    camera.stopRecording()
+                  } else {
+                    const video = camera.recordAsync()
+                    setIsRecording(true)
+                  }
+                }
+              }}
+              style={isRecording ? styles.stopRecordButton : styles.startRecordButton}
+            />
+            <FontAwesome.Button
+              name="camera"
+              color="#FFFFFF"
+              onPress={ () => {
+                if(cameraType === Camera.Constants.Type.front) {
+                  setCameraType(Camera.Constants.Type.back)
+                } else {
+                  setCameraType(Camera.Constants.Type.front)
+                }
+              }}
+            />
+          </View>
+          <View>
+            <StopWatch
+              options={options}
+              start={isRecording}
+            />
+            <View>
+              <Text>
+                {props.question}
+              </Text>
+            </View>
           </View>
         </View>
       </Camera>
     </View>
   )
+}
+
+interface TopDisplayProps {
+  isRecording: boolean,
+}
+
+// Use this for the conditional rendering of the stopwatch.
+function TopDisplay(props: TopDisplayProps) {
+  if(props.isRecording) {
+    return (
+      <StopWatch
+        options={options}
+        start={true}
+      />
+    )
+  } else {
+    return (
+      <View>
+        <Text>Create a 3-4 Minute Video</Text>
+      </View>
+    )
+  }
 }
--- src/screens/Home/RecordScreen/styles.tsx
+++ src/screens/Home/RecordScreen/styles.tsx
@@ -6,6 +6,19 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 50
+  },
+  photoButtonCircle: {
+    height: 40,
+    width: 40,
+    borderRadius: 40,
+    borderColor: '#FFFFFF',
+    borderWidth: 5,
+  },
+  photoButton: {
+    height: 30,
+    width: 30,
+    borderRadius: 30,
+    backgroundColor: '#FFFFFF',
   }
 })
 
