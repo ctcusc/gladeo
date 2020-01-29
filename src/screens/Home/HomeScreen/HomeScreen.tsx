093d11f5702e3d1f2ec9a996b42bebeddebc8bfb
Jamie Flores <jamieflores@Jamies-MacBook-Pro-2.local>
2020-01-28 20:13:29 -0800

    newest commit

--- src/screens/Home/HomeScreen/HomeScreen.tsx
+++ src/screens/Home/HomeScreen/HomeScreen.tsx
@@ -27,7 +27,7 @@ export default function HomeScreen(props: Props) {
   const {navigate} = props.navigation
 
   useEffect(() => {
-    fetch(`${BASE_PATH}/api/questions`)
+    fetch('https://9f43903c.ngrok.io/api/questions')
       .then(res => res.json())
       .then(data => {
         setQuestions(data)
