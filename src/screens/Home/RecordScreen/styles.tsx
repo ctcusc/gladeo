82e644cec53e90f3945ebcb553a3ffb1604a1855
Jamie Flores <jamieflores@Jamies-MacBook-Pro-2.local>
2020-01-28 20:25:30 -0800

    saved styles

--- src/screens/Home/RecordScreen/styles.tsx
+++ src/screens/Home/RecordScreen/styles.tsx
@@ -1,24 +1,41 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-  recordButton: {
-    backgroundColor: '#D94077',
-    width: 50,
-    height: 50,
-    borderRadius: 50
-  },
   photoButtonCircle: {
     height: 40,
     width: 40,
     borderRadius: 40,
     borderColor: '#FFFFFF',
     borderWidth: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   photoButton: {
     height: 30,
     width: 30,
     borderRadius: 30,
     backgroundColor: '#FFFFFF',
+  },
+  startRecordButton: {
+    backgroundColor: '#FF3B30',
+    width: 40,
+    height: 40,
+    borderRadius: 40,
+  },
+  stopRecordButton: {
+    width: 16,
+    height: 16,
+    backgroundColor: '#FF3B30',
+    borderRadius: 4,
+  },
+  recordButtonCircle: {
+    height: 48,
+    width: 48,
+    borderRadius: 48,
+    borderColor: '#FFFFFF',
+    borderWidth: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 })
 
