38deb784c898b91035d871723cc9d46456e97fbd
Aliya Petranik <petranik@usc.edu>
2020-02-01 18:49:32 -0800

    lol

--- src/screens/Main/RecordStack/QuestionsScreen/QuestionsScreen.tsx
+++ src/screens/Main/RecordStack/QuestionsScreen/QuestionsScreen.tsx
@@ -27,7 +27,7 @@ export default function QuestionsScreen(props: Props) {
   const {navigate} = props.navigation
 
   useEffect(() => {
-    fetch('https://e9f5cf12.ngrok.io/api/questions')
+    fetch('https://38561c4d.ngrok.io/api/questions')
       .then(res => res.json())
       .then(data => {
         setQuestions(data)
--- src/shared_components/TickingTimer/TickingTimer.tsx
+++ src/shared_components/TickingTimer/TickingTimer.tsx
@@ -3,15 +3,48 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
  
 export default function TickingTimer () {
   const [minutesCounter, setMinutesCounter] = useState('00')
+  const [seconds, setSeconds] = useState(0)
   const [secondsCounter, setSecondsCounter] = useState('00')
-  const [startDisable, setStartDisable] = useState(false)
+  const [isRecording, setRecording] = useState(false)
   const [timer, setTimer] = useState()
 
   useEffect(() => {
-    return function cleanup() {
-      //do nothing
-    }
-  })
+
+    // if(isRecording === true) {
+    const interval = setInterval(() => {
+      console.log('is recording: ', isRecording)
+      setSeconds(seconds => seconds + 1)
+      // if(!isRecording) {
+      //   setSeconds(0)
+      // }
+
+     
+      let num = seconds.toString(),
+        count = minutesCounter
+      console.log('seconds timer:', num)
+      if (Number(secondsCounter) == 59) {
+        count = (Number(minutesCounter) + 1).toString()
+        num = '00'
+      }
+    
+      setMinutesCounter(count.length == 1 ? '0' : count)
+      setSecondsCounter(num.length == 1 ? '0' + num : num)
+    
+       
+    }, 1000)
+  
+      
+    return () => clearInterval(interval) 
+    // } else {
+    //   setSeconds(0)
+    //   return
+    // }
+    
+
+    
+   
+    return
+  }, [])
 
 
   // useEffect(() => {
@@ -32,20 +65,21 @@ export default function TickingTimer () {
   // }, [])
     
   function onButtonStart() {
-    const timer = setInterval(() => {
-      let num = (Number(secondsCounter) + 1).toString(),
-        count = minutesCounter
-            
-      if (Number(secondsCounter) == 59) {
-        count = (Number(minutesCounter) + 1).toString()
-        num = '00'
-      }
+    // setInterval(() => {
+    //   setSeconds(seconds+1)
+    //   let num = seconds.toString(),
+    //     count = minutesCounter
+    //   console.log('seconds timer:', num)
+    //   if (Number(secondsCounter) == 59) {
+    //     count = (Number(minutesCounter) + 1).toString()
+    //     num = '00'
+    //   }
 
-      setMinutesCounter(count.length == 1 ? '0' : count)
-      setSecondsCounter(num.length == 1 ? '0' + num : num)
-    }, 1000)
-    setTimer(timer)
-    setStartDisable(true)
+    //   setMinutesCounter(count.length == 1 ? '0' : count)
+    //   setSecondsCounter(num.length == 1 ? '0' + num : num)
+    // }, 1000)
+    // // setTimer(timer)
+    // setStartDisable(true)
   }
  
  
@@ -66,15 +100,15 @@ export default function TickingTimer () {
   return (
     <View style={styles.MainContainer}>
  
-      <Text style={styles.counterText}>{minutesCounter} : {secondsCounter}</Text>
+      <Text style={styles.counterText}>{minutesCounter} : {seconds}</Text>
  
       <TouchableOpacity
-        onPress={onButtonStart}
+        onPress={() => setRecording(isRecording => !isRecording)}
         activeOpacity={0.6}
-        style={[styles.button, { backgroundColor: startDisable ? '#B0BEC5' : '#FF6F00' }]} 
-        disabled={startDisable} >
+        style={[styles.button, { backgroundColor: isRecording ? '#B0BEC5' : '#FF6F00' }]} 
+      >
  
-        <Text style={styles.buttonText}>START</Text>
+        <Text style={styles.buttonText}>{isRecording ? 'STOP' :'START'} </Text>
  
       </TouchableOpacity>
       {/*
