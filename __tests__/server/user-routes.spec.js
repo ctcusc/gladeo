05102a920bd45a27bd2dd81459fcd423acf7f62e
Jamie Flores <jamieflores@Jamies-MBP-2.attlocal.net>
2020-02-09 00:55:29 -0800

    Added some tests, but need to test that the answered field is correct

--- __tests__/server/user-routes.spec.js
+++ __tests__/server/user-routes.spec.js
@@ -150,3 +150,50 @@ describe('Checks to see if invalid url is handled', () => {
     done()
   })
 })
+
+describe('Checks user questions route', () => {
+  let questionIDs
+  let session
+  beforeAll(async (done) => {
+    // Grab questions to use for verification later
+    const questions = await request.get('/api/questions')
+    questionIDs = (questions.body).map(question => question.ID)
+    done()
+  })
+  // assume user always has at least one answered questions
+  it('should return the nonempty array of the answered questions of user test2', async (done) => {
+    // Setup session and login user
+    session = await supertestsession(app)
+    await session.post('/api/auth/login').send({ Email: 'a2@b.com', Password: 'password'})
+
+    // Check getting user's answered questions is successful
+    const res = await session.get('/api/user/questions')
+    expect(res.status).toBe(200)
+
+    // Check to see if # questions is same as accessed
+    const userQuestions = res.body
+    expect(userQuestions.length).toBe(questions.body.length)
+
+    // Check if each of the recordId is one existing questionId
+    userQuestions.forEach(question => expect(questionIDs.includes(question.ID)).toBe(true))
+
+    // TODO: Check to see if each question is truly answered by the user
+    done()
+  })
+  // assume user always has at least one answered questions
+  it('should return the empty array of the answered questions of user test', async (done) => {
+    // Setup session and login user
+    session = await supertestsession(app)
+    await session.post('/api/auth/login').send({ Email: 'a@b.com', Password: 'password'})
+
+    // Check getting user's answered questions is successful
+    const res = await session.get('/api/user/answered')
+    expect(res.status).toBe(200)
+
+    // Check to see if # answered questions is zero
+    const answeredQuestions = res.body
+    expect(answeredQuestions.length).toBe(0)
+
+    done()
+  })
+}) 
