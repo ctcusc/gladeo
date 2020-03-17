c97d3d83a26b7f664827cebe419115ee9c1c9a0a
Lynne <lynnebai@usc.edu>
2020-03-17 15:12:58 -0400

    merge

---
+++ src/screens/Auth/GetStartedScreen/styles.tsx
@@ -0,0 +1,62 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+
+  main: {
+    flex: 3,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#000',
+  },
+
+  margin: {
+    marginBottom: '16%',
+  },
+
+  normalText: {
+    fontStyle: 'normal',
+    fontFamily: 'roboto-regular',
+    fontSize: 16,
+    lineHeight: 19,
+    color: '#000000',
+  },
+
+  pinkTextButton: {
+    fontStyle: 'normal',
+    fontSize: 16,
+    lineHeight: 19,
+    fontFamily: 'roboto-regular',
+    color: '#E5186E',
+  },
+
+  resendButtonLine: {
+    marginBottom: '4%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '4%'
+  },
+
+  imageLine: {
+    marginTop: '2%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  image: {
+    height: 92,
+  },
+
+  footer: {
+    flex: 1,
+  }
+})
+
+export default styles
---
+++ src/screens/Auth/PasswordResetScreen/PasswordResetScreen.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import {
+  Text,
+  View,
+  Alert,
+  TouchableOpacity,
+} from 'react-native'
+import styles from '../../Auth/PasswordResetScreen/styles'
+import BlackHeading from '../../../shared_components/BlackHeading/BlackHeading'
+import GreyTextInput from '../../../shared_components/GreyTextInput/GreyTextInput'
+import PinkButton from '../../../shared_components/PinkButton/PinkButton'
+
+export default function PasswordResetScreen() {
+  const [email, setEmail] = useState('')
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <BlackHeading title="Reset your Password" />
+        <Text style={styles.regularText}>We&apos;ll send a reset link to the email linked to your account</Text>
+        <GreyTextInput changeTextContent={(email) => {
+          setEmail(email)
+        }} placeholder="Email Address" inputType='emailAddress' input={email}/>
+        <PinkButton title="SEND" onPress={() => Alert.alert('pressed')} disabled={!email}/>
+        <View style={styles.resendButtonLine}> 
+          <Text style={styles.normalText}>No dice?</Text>
+          <TouchableOpacity
+            onPress={() => Alert.alert('pressed')}
+          >
+            <Text style={styles.pinkTextButton}> Resend validation link.</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        
+      </View>
+    </View>
+  )
+}
