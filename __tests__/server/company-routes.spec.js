0371298565422befeb3c1e053c545a7e351eb1ee
Aliya Petranik <petranik@usc.edu>
2020-03-26 14:27:10 -1000

    small error status cleanups (#101)

--- __tests__/server/company-routes.spec.js
+++ __tests__/server/company-routes.spec.js
@@ -3,21 +3,21 @@ const app = require('../../server/api/app')
 const request = supertest(app)
 
 describe('Checks to see if company route returns user data successfully with valid company code', () => {
-  it('should print user data if successful', async (done) => {
+  it('should print user data if successful', async () => {
     const res = await request.get('/api/company/code2')
     expect(res.body['_record']).toBe('recxUAb83anU8A72k')
     expect(res.body['Full Name']).toMatch('test3')
     expect(res.body['Company Code']).toBe('code2')
     expect(res.status).toBe(200)
-    done()
+    // done()
   })
 })
 
 describe('Checks to see if valid company code with a registered user is handled', () => {
-  it('should return a 409 error', async () => {
+  it('should return a 409 error', async (done) => {
     const res = await request.get('/api/company/acefh')
     expect(res.status).toBe(409)
-    // done()
+    done()
   })
 })
 
