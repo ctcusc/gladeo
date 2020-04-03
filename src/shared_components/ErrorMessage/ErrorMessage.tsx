88d8d248e02f9d79d3c230313ae8004891e4838a
Jamie Flores <jamieflores@Jamies-MBP-2.attlocal.net>
2020-04-03 17:40:18 -0400

    Created ErrorMessage component that is implemented on the login page

---
+++ src/shared_components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {
+  Text,
+  View,
+} from 'react-native'
+import styles from './styles'
+
+interface Props {
+  message: string,
+  visible: boolean,
+}
+  
+export default function PinkButton (props: Props) {
+  if(props.visible) {
+    return (
+      <View>
+        <Text
+          style={styles.text}>
+                      Error: {props.message}
+        </Text>
+      </View>
+    )
+  } else {
+    return null
+  }
+}
