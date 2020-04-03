15ed0a346b313ebc794c66356ad7309d3146b2b9
Jamie Flores <jamieflores@Jamies-MBP-2.attlocal.net>
2020-04-03 19:14:18 -0400

    Fixed flex styles so that the images on the onbording screen work well on (hopefully) all devices

--- src/screens/Onboarding/StepScreens/styles.tsx
+++ src/screens/Onboarding/StepScreens/styles.tsx
@@ -4,19 +4,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
+    justifyContent: 'space-evenly',
+    paddingTop: '20%'
   },
     
   header: {
-    flex: 2,
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    justifyContent: 'center'
+    
   },
 
   footer: {
-    flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
   },
   text: {
@@ -27,32 +29,8 @@ const styles = StyleSheet.create({
     lineHeight: 37,
     textAlign: 'center',
     color: 'black',
-    marginTop: '40%',
+    width: 300
   },
-
-  textOne: {
-    marginBottom: '17%',
-    height: 74,
-    width: 256,
-  }, 
-
-  textTwo: {
-    marginBottom: '13%',
-    height: 111,
-    width: 256,
-  }, 
-
-  textThree: {
-    marginBottom: '12%',
-    height: 111,
-    width: 270,
-  }, 
-
-  textFour: {
-    marginBottom: '10%',
-    height: 111,
-    width: 307,
-  }, 
 
   button: {
     width: 179,
@@ -68,7 +46,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#E5186E',
     textAlign: 'center',
-  }
+  },
 })
 
 export default styles
