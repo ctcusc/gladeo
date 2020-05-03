6157dc80bad08c53266948c962384c797726d27d
Aliya Petranik <petranik@usc.edu>
2020-05-02 21:57:19 -1000

    react native module / linking fixes

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import { Platform, StatusBar, StyleSheet, View } from 'react-native'
 import { Provider } from 'react-redux'
 // import {store  from './src/redux/store'
 import {store, persistor} from './src/redux/store'
-import { persistStore } from 'redux-persist'
 
 import { PersistGate } from 'redux-persist/integration/react'
 
--- android/app/src/main/java/com/gladeo_app/MainApplication.java
+++ android/app/src/main/java/com/gladeo_app/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.cameraroll.CameraRollPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.github.yamill.orientation.OrientationPackage;
