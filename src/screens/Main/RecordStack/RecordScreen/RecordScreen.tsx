37d7731a580e136831dafa64c1901bc17cbdcba8
Brenna Chen <brennajc@usc.edu>
2020-05-13 05:47:18 -0700

    last ticket

--- src/screens/Main/RecordStack/RecordScreen/RecordScreen.tsx
+++ src/screens/Main/RecordStack/RecordScreen/RecordScreen.tsx
@@ -11,7 +11,8 @@ import styles from './styles'
 import { NavigationScreenProp, NavigationState, NavigationParams } from 'react-navigation'
 import Video from 'react-native-video'
 import RNConvertPhAsset from 'react-native-convert-ph-asset'
-
+import BackgroundTimer from 'react-native-background-timer'
+import { TIMEOUT } from 'dns'
 
 interface Props {
   dispatch: Function,
@@ -37,6 +38,7 @@ const PendingView = () => (
 )
 
 function RecordScreen(props: Props) {
+  const {navigate} = props.navigation
   const {goBack, pop} = props.navigation
   //const question = props.navigation.state.params.question
   const [hasPermission, setHasPermission] = useState(true)
@@ -46,6 +48,9 @@ function RecordScreen(props: Props) {
   const [video, setVideo] = useState(null)
   const [isPreviewMode, setPreviewMode] = useState(false)
   const [audioCapture, setAudioCapture] = useState(true)
+  const [second, setSecond] = useState(0)
+  const [topText, setTopText] = useState('Create a 3-4 Minute Video')
+  const [playing, setPlaying] = useState(false)
 
   const question = props.navigation.state.params.question
   const questionID = props.navigation.state.params.questionID
@@ -54,6 +59,15 @@ function RecordScreen(props: Props) {
     const [screenData, setScreenData] = useState(Dimensions.get('screen'))
   
     useEffect(() => {
+      if(playing == true) {
+        setTopText(second.toString())
+      }
+      const timer =
+        setInterval(() => setSecond(second + 1), 1000)
+      return () => clearInterval(timer)
+    }, [second])
+
+    useEffect(() => {
       const onChange = result => {
         setScreenData(result.screen)
       }
@@ -61,7 +75,7 @@ function RecordScreen(props: Props) {
       Dimensions.addEventListener('change', onChange)
   
       return () => Dimensions.removeEventListener('change', onChange)
-    })
+    }, )
   
     return {
       ...screenData,
@@ -122,11 +136,15 @@ function RecordScreen(props: Props) {
 
   async function stopRecord(){
     camera.stopRecording()
+    setPlaying(false)
+    setSecond(0)
   }
 
   async function startRecord () {
     if (camera) {
       try {
+        setPlaying(true)
+        setSecond(0)
         setIsRecording(true)
         const data = await camera.recordAsync()
 
@@ -187,10 +205,10 @@ function RecordScreen(props: Props) {
           
           <View style={styles.texts}>
             <View>
-              <Text style={styles.infoText}>Create a 3-4 Minute Video</Text>
+              <Text style={styles.infoText}>{topText}</Text>
             </View>
             <View>
-              <Text style={styles.question}>{/*question*/}Question will go here</Text>
+              <Text style={styles.question}>{question}</Text>
             </View>
           </View>
           <View style={styles.controls}>
@@ -223,7 +241,8 @@ function RecordScreen(props: Props) {
         </View>
       </RNCamera>
     )
-  } 
+  }
+
   if(isPreviewMode) {
     return (
       <View style={styles.videoPlay}>
@@ -237,16 +256,40 @@ function RecordScreen(props: Props) {
           isLooping={true}
           style={{ width: '100%', height: '100%' }}
         />
-        <View style={ styles.videoBottom }>
+  
+       
+        <View style={styles.videoTop}>
           <TouchableOpacity
-            onPress={()=>save()}
-            style={styles.saveButton}
+            onPress={() => {
+              Alert.alert(
+                'Confirm Deletion?',
+                'This video will not be recoverable. You will return to snippet selections.',
+                [
+                  {text: 'Delete',
+                    onPress: () => {
+                      setTopText('Create a 3-4 Minute Video')
+                      setPreviewMode(false)
+                    }
+                  },
+                  {text: 'Cancel', style: 'cancel'}
+                ]
+              )
+            }}
           >
-            <Text style={styles.saveText}>save</Text>
+            <Image resizeMode='contain' source={require('../../../../../assets/images/xmark.png')} style = {{tintColor: 'white'}}/>
           </TouchableOpacity>
         </View>
-      </View>
-    )
+
+
+          
+        <View style={styles.videoBottom}></View>
+        <TouchableOpacity
+          onPress={()=>save()}
+          style={styles.saveButton}
+        >
+          <Text style={styles.saveText}>SAVE</Text>
+        </TouchableOpacity>
+      </View>  )
   } else {
     return <PendingView />
   }
--- src/screens/Main/RecordStack/ViewScreen/ViewScreen.tsx
+++ src/screens/Main/RecordStack/ViewScreen/ViewScreen.tsx
@@ -1,25 +1,49 @@
 import React, {useEffect, useState} from 'react'
-import { Text, View, TouchableOpacity} from 'react-native'
+import { Text, View, TouchableOpacity, Alert, Image} from 'react-native'
 import styles from './styles'
 import Video from 'react-native-video'
+import { BASE_PATH } from 'react-native-dotenv'
 
 import { NavigationScreenProp, NavigationState, NavigationParams } from 'react-navigation'
 
 interface Props {
+  questionID: number,
   question: string,
   uri: string,
   navigation: NavigationScreenProp<NavigationState, NavigationParams>,
 }
 
 export default function ViewScreen(props: Props) {
-  const {goBack} = props.navigation
+  const {navigate} = props.navigation
+  const {goBack, pop} = props.navigation
   const question = props.navigation.state.params.question
+  const questionID = props.navigation.state.params.questionID
   const uri = props.navigation.state.params.uri
 
-  return(
+  async function removeQuestion(id: number){
+    fetch(`${BASE_PATH}/api/user/questions`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        'questionId': id,
+      }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.log('Error: ' + error)
+      })
+  }
+
+  return (
     <View style={styles.videoPlay}>
       <Video
-        source={{ uri: uri }}
+        source={{ uri: video.uri }}
         rate={1.0}
         volume={1.0}
         isMuted={false}
@@ -28,6 +52,51 @@ export default function ViewScreen(props: Props) {
         isLooping={true}
         style={{ width: '100%', height: '100%' }}
       />
+
+       
+      <View style={styles.videoTop}>
+        <TouchableOpacity
+          onPress={() => {
+            Alert.alert(
+              'Confirm Deletion?',
+              'This video will not be recoverable. You will return to snippet selections.',
+              [
+                {text: 'Delete',
+                  onPress: () => {
+                    removeQuestion(questionID)
+                    pop()
+                  }
+                },
+                {text: 'Cancel', style: 'cancel'}
+              ]
+            )
+          }}
+        >
+          <Image resizeMode='contain' source={require('../../../../../assets/images/xmark.png')} style = {{tintColor: 'white'}}/>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.videoBottom}></View>
+      <Text style={styles.questionFinalText}>{question}</Text>
+      <TouchableOpacity
+        onPress={() => {
+          Alert.alert(
+            'Confirm Submission?',
+            'Are you sure you\'re ready to submit the video for review?',
+            [
+              {text: 'Submit',
+                onPress: () => {
+                  navigate('UploadingVideo')
+                }
+              },
+              {text: 'Cancel', style: 'cancel'}
+            ]
+          )
+        }}
+        style={styles.saveButton}
+      >
+        <Text style={styles.saveText}>SUBMIT</Text>
+      </TouchableOpacity>
     </View>
   )
 }
